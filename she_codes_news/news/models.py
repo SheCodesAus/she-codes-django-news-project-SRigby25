f7ba5850a43fb8459555332af802b3659322d052
SRigby25 <sian.rigby@hotmail.com.au>
2022-02-05 12:48:39 +0800

    sat 5/2 .3

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    image = models.URLField(default="https://picsum.photos/600")
+    # author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     content = models.TextField()
+    
+
